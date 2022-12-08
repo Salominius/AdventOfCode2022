061d5ea51ba284dac4b30b9f190f4e836aee134b
Arne Malaka <a12028537@unet.univie.ac.at>
2022-12-08 13:55:44 +0100

    Day8, but way prettier

--- Day8.py
+++ Day8.py
@@ -1,80 +1,51 @@
 from helpers.importHelpers import *
 
+def getRanges(treeRow, treeColumn, height, width): #returns 4 lists of tuples (row, column) for the 4 directions
+  # tree -> right side:
+  yield [(row, treeColumn) for row in range(treeRow+1, width)]
+  # tree -> left side:
+  yield [(row, treeColumn) for row in range(treeRow-1, -1, -1)]
+  # tree -> top side:
+  yield [(treeRow, column) for column in range(treeColumn-1, -1, -1)]
+  # tree -> bottom side:
+  yield [(treeRow, column) for column in range(treeColumn+1, height)]
+
 def getVisibleTrees(trees):
-  # 0 = not visible, 1 = visible
-  visibleTrees = [[0 for _ in row] for row in trees]
-  #left to right
-  for row in range(len(trees)):
-    visibleHeight = -1
-    for column in range(len(trees[0])):
-      if trees[row][column] > visibleHeight:
-        visibleTrees[row][column] = 1
-        visibleHeight = trees[row][column]
-  # top to bottom
-  for column in range(len(trees[0])):
-    visibleHeight = -1
-    for row in range(len(trees)):
-      if trees[row][column] > visibleHeight:
-        visibleTrees[row][column] = 1
-        visibleHeight = trees[row][column]
-  # right to left
-  for row in range(len(trees)):
-    visibleHeight = -1
-    for column in range(len(trees[0])-1, -1, -1):
-      if trees[row][column] > visibleHeight:
-        visibleTrees[row][column] = 1
-        visibleHeight = trees[row][column]
-  # bottom to top
-  for column in range(len(trees[0])-1, -1, -1):
-    visibleHeight = -1
-    for row in range(len(trees)-1, -1, -1):
-      if trees[row][column] > visibleHeight:
-        visibleTrees[row][column] = 1
-        visibleHeight = trees[row][column]
-  return visibleTrees 
+  height = len(trees)
+  width = len(trees[0])
+  visibleTreesCounter = 2*height + 2*width - 4 #the trees on the edges are all visible
+  for row in range(1, height - 1):
+    for column in range(1, width - 1):
+      # --- for every tree within the grid, we check if it is visible from the 4 directions ---
+      for direction in getRanges(row, column, height, width): #for each direction, we check if there is a higher tree
+        for eachCoordinate in direction:
+          if trees[eachCoordinate[0]][eachCoordinate[1]] >= trees[row][column]:
+            break #if a tree is higher than the current tree, stop checking this direction
+        else: #if no break occured, that means the tree is visible from that direction
+          visibleTreesCounter += 1  # so we add 1 to the counter,
+          break                     # ... and we can stop checking the other directions
+  return visibleTreesCounter
 
 def getScenicScore(trees):
-  scenicScore =[[0 for _ in row] for row in trees]
-  for row in range(len(trees)):
-    for column in range(len(trees[0])):
-      # --- check up, down, left, right ---
-
-      # up
-      directionScore = 0
-      for up in range (1, row+1):
-        directionScore += 1
-        if trees[row-up][column] >= trees[row][column]:
-          break
-      scenicScore[row][column] = directionScore
-
-      # down
-      directionScore = 0
-      for down in range (1, len(trees)-row):
-        directionScore += 1
-        if trees[row+down][column] >= trees[row][column]:
-          break
-      scenicScore[row][column] *= directionScore
-
-      # left
-      directionScore = 0
-      for left in range (1, column+1):
-        directionScore += 1
-        if trees[row][column-left] >= trees[row][column]:
-          break
-      scenicScore[row][column] *= directionScore
-
-      # right
-      directionScore = 0
-      for right in range (1, len(trees[0])-column):
-        directionScore += 1
-        if trees[row][column+right] >= trees[row][column]:
-          break
-      scenicScore[row][column] *= directionScore    
-  return max(max(row) for row in scenicScore)
+  height = len(trees)
+  width = len(trees[0])
+  maxScenicScore = 0
+  for row in range(height):
+    for column in range(width):
+       # --- for every tree, we calculate its scenic score ---
+      thisScenicScore = 1
+      for direction in getRanges(row, column, height, width): #count how many trees are visible for each direction
+        treesInDirection = 0
+        for eachCoordinate in direction:
+          treesInDirection += 1
+          if trees[eachCoordinate[0]][eachCoordinate[1]] >= trees[row][column]:
+            break
+        thisScenicScore *= treesInDirection
+      if thisScenicScore > maxScenicScore: #update max scenic score if nessecary
+        maxScenicScore = thisScenicScore
+  return maxScenicScore        
         
-stringInput = getInput()
-trees = [[int(char) for char in [*line]] for line in stringInput.split("\n")]
+trees = [[int(digit) for digit in [*row]] for row in getInput().split("\n")]
 
-visibleTrees = getVisibleTrees(trees)
-print("Part 1: ", sum(sum(visibleTrees, [])))
+print("Part 1: ", getVisibleTrees(trees))
 print("Part 2: ", getScenicScore(trees))
