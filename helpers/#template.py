104676874e8fbbc008330e37c4de10f7514bf8da
Arne Malaka <a12028537@unet.univie.ac.at>
2022-12-11 17:03:29 +0100

    updated Template

--- helpers/#template.py
+++ helpers/#template.py
@@ -1,10 +1,8 @@
 from helpers.importHelpers import *
 
-resultPart1 = 0
-resultPart2 = 0
 stringInput = getInput()
 for line in stringInput.split("\n"):
   continue
 
-print("Part 1: ", resultPart1)
-print("Part 2: ", resultPart2)
+print("Part 1: ", 0)
+print("Part 2: ", 0)
