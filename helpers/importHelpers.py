604d087a681a22141492beb77b6a98cd0654cdb9
Arne Malaka <a12028537@unet.univie.ac.at>
2022-12-24 16:39:10 +0100

    Added regex-usage to get day from filename

--- helpers/importHelpers.py
+++ helpers/importHelpers.py
@@ -1,10 +1,8 @@
 import sys
+import re
 
 def getInput(noStrip = False): #get input from file
   fileName = sys.argv[0] #get filename
-  if fileName[-5: -3].isdigit(): #check if day number is 2 digits long (ugly solution for now)
-    day = fileName[-5: -3] #get day number from filename
-  else:
-    day = fileName[-4: -3] #get day number from filename  
+  day = re.findall(r"\d+", fileName)[-1] #get day number from filename
   with open(f'./inputs/input{day}.txt') as inputFile:
     return inputFile.read() if noStrip else inputFile.read().strip()
