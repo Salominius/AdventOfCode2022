fbd79ee7e1f4a5a24ff0eb7cc71c7fc078f886e4
Arne Malaka <a12028537@unet.univie.ac.at>
2022-12-25 15:15:01 +0100

    Day25

--- Day25.py
+++ Day25.py
@@ -1,45 +1,21 @@
 from helpers.importHelpers import *
 
 getDecimal = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
-getSNAFU = {-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
 
-stringInput = getInput()
-requiredFuel = 0
-for line in stringInput.split("\n"):
-  for rank, digit in enumerate(line[-1::-1]):
-    requiredFuel += getDecimal[digit] * 5**rank
+def getRequiredFuel(stringInput):
+  requiredFuel = 0
+  for line in stringInput.split("\n"):
+    for rank, digit in enumerate(line[-1::-1]): # itereate from right to left through every line
+      requiredFuel += getDecimal[digit] * 5**rank # add the value of each digit*rankValue to the total fuel
+  return requiredFuel
 
-highestRank = 0
-highestNumberRepresentable = 0
-while highestNumberRepresentable < requiredFuel: #2 is the highest value we can put every rank
-  highestNumberRepresentable += 2 * 5**highestRank
-  highestRank += 1
+def convertToSnafu(number):
+  result = []
+  while number > 0:
+    number += 2 # if the number is increased by 2 at every rank, we can later subtract 2 from each digit to get the SNAFU-notation
+    result.insert(0, number % 5) # insert smallest rank digit of current number in front of list
+    number = number // 5 # get rid of the smallest rank digit (// is floor division) (just as //10 removes smallest rank digit for base 10)
+  return "".join(map(lambda x: "=-012"[x], result)) # subtracts 2 from each digit and converts to SNAFU-notation
 
-def getHighestNumberRepresentable(rank):
-  number = 0
-  while rank >= 0:
-    number += 2 * 5**rank
-    rank-=1
-  return number
-
-resultPart1 = ""
-currentNumber = 0
-for rank in range(highestRank-1, -1, -1):
-  highestNumberBelow = getHighestNumberRepresentable(rank-1)
-  if currentNumber + highestNumberBelow + 5**rank < requiredFuel:
-    currentNumber += 2*5**rank
-    resultPart1 += "2"
-  elif currentNumber + highestNumberBelow < requiredFuel:
-    currentNumber += 5**rank
-    resultPart1 += "1"
-  elif currentNumber - 5**rank + highestNumberBelow < requiredFuel:
-    resultPart1 += "0"
-  elif currentNumber + -2*5**rank + highestNumberBelow < requiredFuel:
-    currentNumber += -5**rank
-    resultPart1 += "-"
-  else:
-    currentNumber += -2*5**rank
-    resultPart1 += "="
-
-print("Part 1: ", resultPart1)
+print("Part 1: ", convertToSnafu(getRequiredFuel(getInput())))
 print("Part 2: ", 0)
